dfc9a6e305cbdd81f0196db0e581f1d1b7300b79
O's <dhtjstlr777@gmail.com>
2020-12-04 20:23:33 +0900

    Update run_server.py
    그냥 확인용

--- server_dev/run_server.py
+++ server_dev/run_server.py
@@ -5,18 +5,21 @@ app = Flask(__name__)
 server = Server()
 
 @app.route('/request/issue-key', methods=['POST'])
+# 키 발급
 def issue_key():
     req = request.get_json()
     response = server.issue_key(req["id"], req["type"])
     return jsonify(response)
 
 @app.route('/request/get-sign', methods=['POST'])
+# 서명하기, 서명한 값 리턴
 def get_sign():
     req = request.get_json()
     response = server.sign_msg(req["body"], req["usk"], req["type"])
     return jsonify(response)
 
 @app.route('/request/find-signatory', methods=['POST'])
+# 서명한 사람의 아이디.
 def open_sign():
     req = request.get_json()
     response = server.open_sign(req["sign"], req["type"])
