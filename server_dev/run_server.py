0b8dd354b2a1d310b5a518325b022ce61b96de3a
Pseung-min <psm345601@naver.com>
2020-12-15 18:00:30 +0900

    Store used key

--- server_dev/run_server.py
+++ server_dev/run_server.py
@@ -16,12 +16,6 @@ def issue_key():
     response = server.issue_key(req["id"], req["type"])
     return jsonify(response)
 
-# @app.route('/request/get-sign', methods=['POST'])
-# def get_sign():
-#     req = request.get_json()
-#     response = server.sign_msg(req["body"], req["usk"], req["type"])
-#     return jsonify(response)
-
 @app.route('/request/find-signatory', methods=['POST'])
 def open_sign():
     req = request.get_json()
